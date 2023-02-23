1fd7ff13f2c80f1e518b2f88f665faf1782658fa
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-23 19:20:54 +0530

    Program for calculating stock value

---
+++ DetailOfStock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class DetailOfStock
+    {
+        public string NameOfStock;
+        public int Share;
+        public int Price;
+    }
+    public class StockDetails
+    {
+        public List<DetailOfStock> Stock;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,27 @@
     class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome to Stock Account Management");
+
+            string path = "C:\\Users\\Mansi Mohan\\source\\repos\\StockAccountManagement\\StockAccountManagement\\Stock.json";
+
+            StockFetching fetchStock = new StockFetching();
+
+            StockDetails data = fetchStock.Read(path);
+
+            Console.WriteLine("\nSTOCK VALUE: ");
+
+            for (int i = 0; i < data.Stock.Count; i++)
+            {
+                Console.WriteLine("\n" + data.Stock[i].NameOfStock);
+                Console.WriteLine("shares: " + data.Stock[i].Share);
+                Console.WriteLine("price: " + data.Stock[i].Price);
+
+                int TotalValue = data.Stock[i].Share * data.Stock[i].Price;
+                Console.WriteLine("\nThe Total Value of {0} for {1} shares is: {2} ", data.Stock[i].NameOfStock, data.Stock[i].Share, TotalValue);
+                Console.WriteLine("-------------------------------");
+            }
         }
     }
 }
