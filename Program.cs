e9f06ad64f38e99aeb8531f55b2b84931a878b12
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-23 18:53:05 +0530

    Welcome to Stock account management

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StockAccountManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stock Account Management");
+        }
+    }
+}
